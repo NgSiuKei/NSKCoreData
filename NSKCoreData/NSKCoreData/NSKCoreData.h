c0c004cab77b777ca9dbdef4863bad6ed2ae62f4
NgSiuKei <107091842+NgSiuKei@users.noreply.github.com>
2023-10-09 22:53:09 +0800

    v0.1

---
+++ NSKCoreData/NSKCoreData/NSKCoreData.h
@@ -0,0 +1,18 @@
+//
+//  NSKCoreData.h
+//  NSKCoreData
+//
+//  Created by NuSiuKei on 2023/10/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSKCoreData.
+FOUNDATION_EXPORT double NSKCoreDataVersionNumber;
+
+//! Project version string for NSKCoreData.
+FOUNDATION_EXPORT const unsigned char NSKCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSKCoreData/PublicHeader.h>
+#import <NSKCoreData/CoreDataManager.h>
+
