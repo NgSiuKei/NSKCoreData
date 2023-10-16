d3278d73e0e785f115e27f1575e81bf8be76e85a
NgSiuKei <835474695@qq.com>
2023-10-16 23:12:45 +0800

    Change the method to asynchronous.

--- NSKCoreData/NSKCoreData/CoreDataManager.m
+++ NSKCoreData/NSKCoreData/CoreDataManager.m
@@ -10,6 +10,7 @@
 #import <CoreData/NSEntityDescription.h>
 #import <CoreData/NSFetchRequest.h>
 #import <CoreData/NSPropertyDescription.h>
+#import <Coredata/NSPersistentStoreResult.h>
 
 @interface CoreDataManager ()
 
@@ -41,47 +42,53 @@
 }
 
 - (void)createEntity:(NSString *)name editBlock:(void(^)(NSManagedObject * _Nonnull entity))editBlock finishBlock:(void(^ _Nullable)(BOOL isSuccess))finishBlock {
-    @synchronized(self) {
+//    @synchronized(self) {
         if(!self.container) {
             CDMLog(@"[%@] Fail: The container is nil.", name);
             if(finishBlock) {
                 finishBlock(NO);
             }
+            return;
         }
         
-        NSManagedObjectContext *context = self.container.viewContext;
+        __block NSManagedObjectContext *context = self.container.newBackgroundContext;//self.container.viewContext;
         NSManagedObject *entity = [NSEntityDescription insertNewObjectForEntityForName:name inManagedObjectContext:context];
         editBlock(entity);
         
-        if(context.hasChanges) {
-            if([context save:nil]) {
-                CDMLog(@"[%@] Success.", name);
-                
-                if(finishBlock) {
-                    finishBlock(YES);
+        [context performBlock:^{
+            if(context.hasChanges) {
+                if([context save:nil]) {
+                    CDMLog(@"[%@] Success. (thread = %@)", name, [NSThread currentThread]);
+                    
+                    if(finishBlock) {
+                        finishBlock(YES);
+                    }
+                    return;
+                }
+                else {
+                    CDMLog(@"[%@] Fail: Fail to save.", name);
                 }
             }
             else {
-                CDMLog(@"[%@] Fail: Fail to save.", name);
+                CDMLog(@"[%@] Fail: The context hasn't changes.", name);
             }
-        }
-        else {
-            CDMLog(@"[%@] Fail: The context hasn't changes.", name);
-        }
-        
-        if(finishBlock) {
-            finishBlock(NO);
-        }
-    }
+            
+            if(finishBlock) {
+                finishBlock(NO);
+            }
+            return;
+        }];
+//    }
 }
 
 - (void)readEntity:(NSString *)name format:(NSString * _Nullable)format finishBlock:(void(^ _Nullable)(BOOL isSuccess, NSArray * _Nullable entities))finishBlock {
-    @synchronized(self) {
+//    @synchronized(self) {
         if(!self.container) {
             CDMLog(@"[%@] Fail: The container is nil.", name);
             if(finishBlock) {
                 finishBlock(NO, nil);
             }
+            return;
         }
         
         NSManagedObjectContext *context = self.container.viewContext;
@@ -90,23 +97,36 @@
             fetch.predicate = [NSPredicate predicateWithFormat:format];
         }
         
-        NSArray *entities = [context executeFetchRequest:fetch error:nil];
-        if(entities) {
-            CDMLog(@"[%@] Success: The format is \"%@\".", name, format);
-            if(finishBlock) {
-                finishBlock(YES, entities);
+        NSAsynchronousFetchRequest *asynFetch = [[NSAsynchronousFetchRequest alloc] initWithFetchRequest:fetch completionBlock:^(NSAsynchronousFetchResult * _Nonnull result) {
+//            [result.finalResult enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//                CDMLog(@"asyn result: obj = %@", obj);
+//            }];
+            if(result.finalResult) {
+                finishBlock(YES, result.finalResult);
             }
-        }
+            else {
+                finishBlock(NO, nil);
+            }
+        }];
+        [context executeRequest:asynFetch error:nil];
         
-        CDMLog(@"[%@] Fail", name);
-        if(finishBlock) {
-            finishBlock(NO, nil);
-        }
-    }
+//        NSArray *entities = [context executeFetchRequest:fetch error:nil];
+//        if(entities) {
+//            CDMLog(@"[%@] Success: The format is \"%@\".", name, format);
+//            if(finishBlock) {
+//                finishBlock(YES, entities);
+//            }
+//        }
+//
+//        CDMLog(@"[%@] Fail", name);
+//        if(finishBlock) {
+//            finishBlock(NO, nil);
+//        }
+//    }
 }
 
 - (void)updateEntity:(NSString *)name format:(NSString * _Nullable)format editBlock:(void(^)(NSManagedObject * _Nonnull entity))editBlock finishBlock:(void(^ _Nullable)(BOOL isSuccess))finishBlock {
-    @synchronized(self) {
+//    @synchronized(self) {
         if(!self.container) {
             CDMLog(@"[%@] Fail: The container is nil.", name);
             if(finishBlock) {
@@ -114,84 +134,149 @@
             }
         }
         
-        NSManagedObjectContext *context = self.container.viewContext;
+        __block NSManagedObjectContext *context = self.container.viewContext;
         NSFetchRequest *fetchGroup = [[NSFetchRequest alloc] initWithEntityName:name];
         if(format) {
             fetchGroup.predicate = [NSPredicate predicateWithFormat:format];
         }
         
-        NSArray *entities = [context executeFetchRequest:fetchGroup error:nil];
-        if(entities) {
-            for(id entity in entities) {
-                editBlock(entity);
-            }
-            
-            if(context.hasChanges && [context save:nil]) {
-                CDMLog(@"[%@] Success: The format is \"%@\".", name, format);
-                if(finishBlock) {
-                    finishBlock(YES);
+        NSAsynchronousFetchRequest *asynFetch = [[NSAsynchronousFetchRequest alloc] initWithFetchRequest:fetchGroup completionBlock:^(NSAsynchronousFetchResult * _Nonnull result) {
+            if(result.finalResult) {
+//                for(id entity in result.finalResult) {
+//                    editBlock(entity);
+//                }
+                if(editBlock) {
+                    [result.finalResult enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                        editBlock(obj);
+                    }];
                 }
-            }
-            else if(!context.hasChanges) {
-                CDMLog(@"[%@] Fail: The context hasn't changes.", name);
+                
+                [context performBlock:^{
+                    if(context.hasChanges && [context save:nil]) {
+                        CDMLog(@"[%@] Success: The format is \"%@\".", name, format);
+                        if(finishBlock) {
+                            finishBlock(YES);
+                        }
+                    }
+                    else if(!context.hasChanges) {
+                        CDMLog(@"[%@] Fail: The context hasn't changes.", name);
+                    }
+                    else {
+                        CDMLog(@"[%@] Fail: Fail to save.", name);
+                    }
+                }];
             }
             else {
-                CDMLog(@"[%@] Fail: Fail to save.", name);
+                CDMLog(@"[%@] Fail: No entity matching the format \"%@\" could be found.", name, format);
+                if(finishBlock) {
+                    finishBlock(NO);
+                }
             }
-        }
-        else {
-            CDMLog(@"[%@] Fail: No entity matching the format \"%@\" could be found.", name, format);
-        }
+        }];
+        [context executeRequest:asynFetch error:nil];
         
-        if(finishBlock) {
-            finishBlock(NO);
-        }
-    }
+//        NSArray *entities = [context executeFetchRequest:fetchGroup error:nil];
+//        if(entities) {
+//            for(id entity in entities) {
+//                editBlock(entity);
+//            }
+//
+//            if(context.hasChanges && [context save:nil]) {
+//                CDMLog(@"[%@] Success: The format is \"%@\".", name, format);
+//                if(finishBlock) {
+//                    finishBlock(YES);
+//                }
+//            }
+//            else if(!context.hasChanges) {
+//                CDMLog(@"[%@] Fail: The context hasn't changes.", name);
+//            }
+//            else {
+//                CDMLog(@"[%@] Fail: Fail to save.", name);
+//            }
+//        }
+//        else {
+//            CDMLog(@"[%@] Fail: No entity matching the format \"%@\" could be found.", name, format);
+//        }
+//
+//        if(finishBlock) {
+//            finishBlock(NO);
+//        }
+//    }
 }
 
 - (void)deleteEntity:(NSString *)name format:(NSString * _Nullable)format finishBlock:(void(^ _Nullable)(BOOL isSuccess))finishBlock {
-    @synchronized(self) {
+//    @synchronized(self) {
         if(!self.container) {
             CDMLog(@"[%@] Fail: The container is nil.", name);
             if(finishBlock) {
                 finishBlock(NO);
             }
+            return;
         }
         
-        NSManagedObjectContext *context = self.container.viewContext;
+        __block NSManagedObjectContext *context = self.container.newBackgroundContext;//self.container.viewContext;
         NSFetchRequest *fetch = [[NSFetchRequest alloc] initWithEntityName:name];
         if(format) {
             fetch.predicate = [NSPredicate predicateWithFormat:format];
         }
         
-        NSArray *entities = [context executeFetchRequest:fetch error:nil];
-        if(entities) {
-            for(id entity in entities) {
-                [context deleteObject:entity];
-            }
-            
-            if(context.hasChanges && [context save:nil]) {
-                CDMLog(@"[%@] Success: The format is \"%@\".", name, format);
-                if(finishBlock) {
-                    finishBlock(YES);
+        NSAsynchronousFetchRequest *asynFetch = [[NSAsynchronousFetchRequest alloc] initWithFetchRequest:fetch completionBlock:^(NSAsynchronousFetchResult * _Nonnull result) {
+            if(result.finalResult) {
+                for(id entity in result.finalResult) {
+                    [context deleteObject:entity];
                 }
-            }
-            else if(!context.hasChanges) {
-                CDMLog(@"[%@] Fail: The context hasn't changes.", name);
+                
+                [context performBlock:^{
+                    if(context.hasChanges && [context save:nil]) {
+                        CDMLog(@"[%@] Success: The format is \"%@\". (thread = %@)", name, format, [NSThread currentThread]);
+                        if(finishBlock) {
+                            finishBlock(YES);
+                        }
+                    }
+                    else if(!context.hasChanges) {
+                        CDMLog(@"[%@] Fail: The context hasn't changes.", name);
+                    }
+                    else {
+                        CDMLog(@"[%@] Fail: Fail to save.", name);
+                    }
+                }];
             }
             else {
-                CDMLog(@"[%@] Fail: Fail to save.", name);
+                finishBlock(NO);
             }
-        }
-        else {
-            CDMLog(@"[%@] Fail: No entity matching the format \"%@\" could be found.", name, format);
-        }
+        }];
+        [context executeRequest:asynFetch error:nil];
         
-        CDMLog(@"[%@] Fail", name);
-        if(finishBlock) {
-            finishBlock(NO);
-        }
-    }
+//        NSArray *entities = [context executeFetchRequest:fetch error:nil];
+//        if(entities) {
+//            for(id entity in entities) {
+//                [context deleteObject:entity];
+//            }
+//
+//            if(context.hasChanges && [context save:nil]) {
+//                CDMLog(@"[%@] Success: The format is \"%@\". (thread = %@)", name, format, [NSThread currentThread]);
+//                if(finishBlock) {
+//                    finishBlock(YES);
+//                }
+//                return;
+//            }
+//            else if(!context.hasChanges) {
+//                CDMLog(@"[%@] Fail: The context hasn't changes.", name);
+//            }
+//            else {
+//                CDMLog(@"[%@] Fail: Fail to save.", name);
+//            }
+//        }
+//        else {
+//            CDMLog(@"[%@] Fail: No entity matching the format \"%@\" could be found.", name, format);
+//        }
+//
+//        CDMLog(@"[%@] Fail", name);
+//        if(finishBlock) {
+//            finishBlock(NO);
+//        }
+//        return;
+//    }
 }
 
 - (void)parseEntities {
